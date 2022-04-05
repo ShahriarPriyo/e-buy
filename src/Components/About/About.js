89c0169826a4424603896134da6983b6162fe862
ShahriarPriyo <shahriarpriyo98@gmail.com>
2022-04-05 11:43:52 +0600

    about page complete

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>This is about page</h1>
+        <div class='conatiner mx-auto py-5 px-5'>
+            <h1 className='text-danger text-center'>About Us</h1>
+            <p>We are trying to build a community where everyone will be able to buy their product very easily. We have a large amount of delivery persons of our own. In order to build trust among customers we check the quality of the products twice through our quality assurance team. They are working hard to find the best quality product. We also have a Review group. They continously listen to the customers and try to understand their feedbacks.</p>
+
         </div>
     );
 };
